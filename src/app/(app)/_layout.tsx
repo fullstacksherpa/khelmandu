cc86e602f33b017fb629840c7653ce2dc5ba40e8
Ongchen Sherpa <fullstacksherpa@gmail.com>
2024-10-27 20:24:51 -0700

    ui for home

--- src/app/(app)/_layout.tsx
+++ src/app/(app)/_layout.tsx
@@ -1,15 +1,18 @@
 /* eslint-disable react/no-unstable-nested-components */
+import Ionicons from '@expo/vector-icons/AntDesign';
 import { Link, Redirect, SplashScreen, Tabs } from 'expo-router';
 import React, { useCallback, useEffect } from 'react';
+import { Image } from 'react-native';
 
 import { useAuth, useIsFirstTime } from '@/core';
-import { Pressable, Text } from '@/ui';
+import { Pressable, Text, View } from '@/ui';
 import {
   Feed as FeedIcon,
   Settings as SettingsIcon,
   Style as StyleIcon,
 } from '@/ui/icons';
 
+// eslint-disable-next-line max-lines-per-function
 export default function TabLayout() {
   const status = useAuth.use.status();
   const [isFirstTime] = useIsFirstTime();
@@ -39,6 +42,39 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <FeedIcon color={color} />,
           headerRight: () => <CreateNewPostLink />,
           tabBarTestID: 'feed-tab',
+        }}
+      />
+      <Tabs.Screen
+        name="home"
+        options={{
+          headerTitle: '',
+          headerLeft: () => (
+            <View className="ml-4 flex-row items-center gap-2">
+              <Text className="text-lg font-medium">Dasarahalli</Text>
+            </View>
+          ),
+          headerRight: ({ tintColor }) => (
+            <View className="mr-4 flex-row items-center gap-3">
+              <Ionicons
+                name="chatbox-outline"
+                size={24}
+                color={tintColor || 'black'}
+              />
+              <Ionicons
+                name="notifications-outline"
+                size={24}
+                color={tintColor || 'black'}
+              />
+              <Pressable onPress={() => {}}>
+                <Image
+                  className="h-8 w-8 rounded-full"
+                  source={{
+                    uri: 'https://accounts.google.com/SignOutOptions?hl=en&continue=https://mail.google.com/mail&service=mail&ec=GBRAFw',
+                  }}
+                />
+              </Pressable>
+            </View>
+          ),
         }}
       />
 
